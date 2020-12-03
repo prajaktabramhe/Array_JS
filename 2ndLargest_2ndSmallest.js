7344a482eb5478fe98fec321b20c71f3e916a5cb
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-12-03 22:16:09 +0530

    added array program files

---
+++ 2ndLargest_2ndSmallest.js
@@ -0,0 +1,17 @@
+var myArray = [];
+
+for (var i = 0; i <= 10; i++) {
+  myArray.push(Math.floor(Math.random() * 900));
+}
+console.log(myArray);
+console.log(Math.max(...myArray));
+console.log(Math.min(...myArray));
+var intArray = myArray.map(Number);
+var second = intArray.sort(function (a, b) {
+  return b - a;
+})[1];
+console.log("second number: " + second);
+var secondLast = intArray.sort(function (a, b) {
+  return a - b;
+})[1];
+console.log("Secondlast number : " + secondLast);
